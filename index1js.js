9b8c337c17017488573ad749d622031ae2e08077
Katie Anderson <31149472+krander@users.noreply.github.com>
2019-03-01 17:33:26 -0500

    Delete index1js.js

--- index1js.js
+++
@@ -1,7 +0,0 @@
-var userBirthday = prompt("When is your birthday?", "mm/dd/yyy")
-var userBirthdayAsDate = new Date(userBirthday)
-var userBirthdayAsNumber = userBirthdayAsDate.getFullYear()
-var currentDate = new Date()
-var currentDateAsNumber = currentDate.getFullYear()
-var approximateBirthdayNumber = currentDateAsNumber - userBirthdayAsNumber
-document.writeln("Your age is " + approximateBirthdayNumber + ".")
