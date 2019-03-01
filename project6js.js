7601f4b3ecc38bf0d540ef0d629193fb7fe36b24
Katie Anderson <31149472+krander@users.noreply.github.com>
2019-03-01 18:46:19 -0500

    Create project6js.js

---
+++ project6js.js
@@ -0,0 +1,17 @@
+    //get numbers
+    var numbers = [];
+    var product = Number(prompt("Please enter a number or -1 to stop entering numbers"));
+    while (product != -1) {
+        numbers.push(product);
+        product = Number(prompt("Please enter a number or -1 to stop entering numbers"));
+    }
+
+    //sum up numbers
+    var total_product = 1;
+    for (var index = 0; index < numbers.length; index++) {
+        total_product *= numbers[index];
+    }
+
+    var multiply = total_product *= product * (-1);
+    document.write("<p>Your numbers are " + numbers.join(", ") + "</p>");
+    document.write("<p>The product of your favorite numbers is " + multiply + ".</p>");
